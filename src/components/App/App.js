1e1a040d3c30328cfac2ef16be55a8e063b12cc1
posi7790 <posi7790@colorado.edu>
2019-10-07 17:42:31 -0600

    Set up App to display form
    Co-authored-by: Jeannie Evans <jeanmarieevans3@gmail.com>

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,23 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import Form from '../Form/Form';
+import './App.scss';
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentContainer: <Form />
+    }
+  }
 
+  render() {
+    return (
+      <>
+        {this.state.currentContainer}
+      </>
+    )
+  }
+
+}
 
 export default App;
