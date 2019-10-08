68a66ded5277bf161e6cdc9ee0cac8ebc0789014
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-10-08 13:12:45 -0600

    Change UserInfo file to a functional component

--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -1,23 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class UserInfo extends Component() {
-  constructor() {
-    super();
-    this.state = {
-      favorites: []
-    }
-  }
-
-  render() {
-    return(
-      <>
-        <h2>Name: {}</h2>
-        <p>Quote: {}</p>
-        <p>Ranking: {}</p>
-        <button>Sign Out</button>
-      </>
-    )
-  }
+const UserInfo = () => {
+  return(
+    <>
+      <h2>Name: {}</h2>
+      <p>Quote: {}</p>
+      <p>Ranking: {}</p>
+      <button>Sign Out</button>
+    </>
+  )
 }
 
 export default UserInfo;
