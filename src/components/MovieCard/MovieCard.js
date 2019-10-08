b6f79cbd54d8fc72da3ebdd495226d42b35ccd12
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-10-08 12:41:02 -0600

    Set up movie card file

---
+++ src/components/MovieCard/MovieCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MovieCard = () => {
+  <div className="movie-card">
+    <h3>Title</h3>
+    <p>Episode</p>
+    <p>Year</p>
+    <button>View Characters</button>
+  </div>
+}
+
+export default MovieCard;
