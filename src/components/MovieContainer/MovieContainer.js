9b4d6686d160ddea50060b057dcc957cdd40c04c
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-10-08 12:37:30 -0600

    Set up MovieContainer file

---
+++ src/components/MovieContainer/MovieContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const MovieContainer = () => {
+  return (
+    <main>
+      <header>
+        <h1>Star Wars Episodes</h1>
+        <UserInfo />
+      </header>
+      <MovieCard />
+    </main>
+  )
+}
+
+export default MovieContainer;
