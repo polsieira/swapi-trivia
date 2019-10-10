d174e082ed7b5f1c2197890395ded204cb43bc11
posi7790 <posi7790@colorado.edu>
2019-10-10 14:19:34 -0600

    Move Router wrap to index.js
    Co-authored-by: Jeannie Evans <jeanmarieevans3@gmail.com>

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { getMovies, getCharacters } from '../../apiCalls';
-import { BrowserRouter as Router, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Form from '../Form/Form';
 import MovieContainer from '../MovieContainer/MovieContainer';
 import CharacterContainer from '../CharacterContainer/CharacterContainer';
@@ -21,30 +21,29 @@ class App extends Component {
       .then(movies => this.setState({ movies }))
       .then(() => console.log('2nd .then() in App', this.state.movies))
       .then(() => getCharacters(this.state.movies[3].characters))
-      .then(results => this.setState({characters: results}))
+      .then(results => this.setState({ characters: results }))
       .catch(error => console.log("Error:", error))
   }
 
   selectMovie = (id) => {
-    this.setState({ movieSelected: id})
+    this.setState({ movieSelected: id })
   }
 
   render() {
     return (
-      <Router>
-        <Route exact path='/' component={ Form } />
-        {this.state.movies &&        
-          <Route 
-            exact path='/movies' 
-            render={() => <MovieContainer movies={this.state.movies}  selectMovie={this.selectMovie}/> }/>
+      <div className="App">
+        <Route exact path='/' component={Form} />
+        {this.state.movies &&
+          <Route
+            exact path='/movies'
+            render={() => <MovieContainer movies={this.state.movies} selectMovie={this.selectMovie} />} />
         }
         {this.state.characters &&
-          <Route 
-            exact path='/movies/3' 
-            render={() => <CharacterContainer characters={this.state.characters}/>}/>
+          <Route
+            exact path='/movies/3'
+            render={() => <CharacterContainer characters={this.state.characters} />} />
         }
-      </Router>
-
+      </div>
     )
   }
 
