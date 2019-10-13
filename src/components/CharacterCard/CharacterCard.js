734a9191395a58f724ff17f1eebc6a90d66c5796
posi7790 <posi7790@colorado.edu>
2019-10-13 16:33:33 -0600

    Make related movies for characters into list items

--- src/components/CharacterCard/CharacterCard.js
+++ src/components/CharacterCard/CharacterCard.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 const CharacterCard = ({ name, homeworld, population, species, films }) => {
+  const filmsList = films.map((film, index) => {
+    return <li key={index}>{film}</li>
+  })
   return (
     <div className="character-card">
       <p>Name: {name}</p>
       <p>Homeworld: {homeworld}</p>
       <p>Population: {population}</p>
-      <p>Species: {species}</p>
-      <p>Related Films: {films}</p>
+      <p>Species: {!species ? 'Species Unkown' : species}</p>
+      <p>Related Films: {filmsList}</p>
       <button>Make a Favorite</button>
     </div>
   )
