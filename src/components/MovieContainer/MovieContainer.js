effbcbb8aa89de4e6bd0c2ed222e17419225268f
posi7790 <posi7790@colorado.edu>
2019-10-13 11:14:31 -0600

    Add keys to movies to git rid of console warning

--- src/components/MovieContainer/MovieContainer.js
+++ src/components/MovieContainer/MovieContainer.js
@@ -3,25 +3,26 @@ import MovieCard from '../MovieCard/MovieCard';
 
 const MovieContainer = (props) => {
   const movieInfo = props.movies.map(movie => {
-      return <MovieCard
-        id = { movie.episode_id }
-        title = { movie.title }
-        episode = { movie.episode_id }
-        year = { movie.release_date }
-        />
-    })
+    return <MovieCard
+      id={movie.episode_id}
+      key={movie.episode_id}
+      title={movie.title}
+      episode={movie.episode_id}
+      year={movie.release_date}
+    />
+  })
 
 
-    return (
-      <main>
-        <header>
-          <h1>Star Wars Episodes</h1>
-          {/* <UserInfo /> */}
-        </header>
-        { movieInfo }
-      </main>
-    )
-  
+  return (
+    <main>
+      <header>
+        <h1>Star Wars Episodes</h1>
+        {/* <UserInfo /> */}
+      </header>
+      {movieInfo}
+    </main>
+  )
+
 }
 
 
