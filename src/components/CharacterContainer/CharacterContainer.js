03f20c67cc4befa05bba3a720c21bc9a023a0899
posi7790 <posi7790@colorado.edu>
2019-10-15 11:42:33 -0600

    Add loading page

--- src/components/CharacterContainer/CharacterContainer.js
+++ src/components/CharacterContainer/CharacterContainer.js
@@ -2,8 +2,20 @@ import React from 'react';
 import CharacterCard from '../CharacterCard/CharacterCard';
 import UserInfo from '../UserInfo/UserInfo';
 import CrawlContainer from '../CrawlContainer/CrawlContainer';
+import './CharacterContainer.scss';
+
 
 const CharacterContainer = ({ characters, user, scrollingText, episode, title }) => {
+
+  if (characters.length === 0) {
+    return (
+      <main className='loadingPage'>
+        <h1>Loading..</h1>
+        <h3>In the meantime watch this Ewok!</h3>
+        <img src={'https://i.imgur.com/ac5hdMu.gif'} alt={'Ewok loading page'} />
+      </main>
+    )
+  }
 
   const characterInfo = characters.map((character, index) => {
     return <CharacterCard
